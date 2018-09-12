1224372aaef83409e2065dc3ff70dc27df7a5364
abdoali28299 <abdoali28299@gmail.com>
2018-09-12 21:27:52 +0200

    final

--- routes/course.js
+++ routes/course.js
@@ -72,18 +72,7 @@ router.post('/comment/:id', ensureAuthenticated, (req, res) => {
     Course
         .findOne({ _id: req.params.id })
         .then(course => {
-            let x = course.rate
-            x = x * (course.comments.length + 1)
-            console.log('total rate before '+x)
-            console.log(req.body.rate)
-            console.log(course.comments.length)
-            x = x + req.body.rate
-            y = course.comments.length + 2
-            console.log('total rate '+x)
-            console.log('total rater '+y)
-            x = x/y
-            console.log(x)
-            course.rate = x
+            course.rate = ((parseInt(course.rate) * (course.comments.length + 1)) + parseInt(req.body.rate))/(course.comments.length + 2)
             let newComment = {
                 commentBody: req.body.commentBody,
                 commentRate: req.body.rate,
