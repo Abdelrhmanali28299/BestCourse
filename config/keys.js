08202fcc088cd317773a032f9b140ebd7c571d49
abdoali28299 <abdoali28299@gmail.com>
2018-09-12 17:50:58 +0200

    add

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
     mongoURI: 'mongodb://abdoali28299:Abdo282990ali@ds235022.mlab.com:35022/best-course',
-    googleClientID: '741116258662-m0e94v7ok74gbkqr171iod868gleiep0.apps.googleusercontent.com',
-    googleClientSecret: '5J5QV5yl2EuMieBlu6lL8Pbo'
+    googleClientID: '666359204351-8954tddmn29d6tq7oq5d53f0bj2nue5j.apps.googleusercontent.com',
+    googleClientSecret: '8DX1WrYW2B60cnlTdnYhZ527'
 }
